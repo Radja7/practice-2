fe97bf749833744bb0d15ee66a208f5d16d79d30
Radja7 <Radja7@users.noreply.github.com>
2018-12-04 12:03:23 +0300

    Правка

---
+++ js/map.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var AUTOR_AVATAR = "img/avatars/user";
+var OFFER_TITLE = ["Большая уютная квартира", "Маленькая неуютная квартира", "Огромный прекрасный дв орец", "Маленький ужасный дворец", "Красивый гостевой домик", "Некрасивый него степриимный домик", "Уютное бунгало далеко от моря", "Неуютное бунгало по коле но в воде"];
+var OFFER_ADDRESS = [0, 1000];
+var OFFER_PRICE = [1000, 1000000];
+var OFFER_TYPE = ["palace", "flat", "house", "bungalo"];
+var OFFER_ROOMS = [0, 5];
+var OFFER_GUESTS = [1, 20];
+var OFFER_CHECKIN = ["12:00", "13:00", "14:00"];
+var OFFER_CHECKOUT = ["12:00", "13:00", "14:00"];
+var OFFER_FEATURES = ["wifi", "di shwasher", "parking", "washer", "elevator", "conditioner"];
